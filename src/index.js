a2e7308deef03767bc2184a47a7cdf322da49510
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-02-25 13:28:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+ReactDOM.render(<h1 style={{color:"red"}}
+>Hello World!</h1>, document.getElementById("root"));
+ 
+/*  style={{color:"red"}}
+eikhane {color:"red"} hocche ekta js object/value jeta style ke dite hobe/required by style property
+
+and that js object
+
+in order for it to be interpreted correctly inside
+
+an HTML element, has to have a set of curly braces around it. 
+
+{ 
+  key: value 
+  color :"red"   --> js object 
+}
+
+*/
